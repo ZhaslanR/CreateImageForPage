0daf9993dbe45ba68f7e452ed75eb993f7830b12
ZhaslanR <zhaslan_12@bk.ru>
2018-01-10 16:37:33 +0600

    Добавьте файлы проекта.

---
+++ NewsPublish/Create.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewsPublish
+{
+    /// <summary>
+    /// Логика взаимодействия для Create.xaml
+    /// </summary>
+    public partial class Create : Page
+    {
+        public Create()
+        {
+            InitializeComponent();
+        }
+
+        private void BrawseButtonClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = @"C:\";
+            dialog.ShowDialog();
+
+            string filePath = dialog.FileName;
+
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri(filePath);
+            image.EndInit();
+
+            imageConteiner.Source = image;
+        }
+    }
+}
